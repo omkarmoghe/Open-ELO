4b226f77a71ba0ac4e399c9123d2e65ce096f7cc
Omkar Moghe <theomkarmoghe@gmail.com>
2023-04-15 18:23:39 -0700

    test

--- test/models/test_environment.rb
+++ test/models/test_environment.rb
@@ -1,12 +1,22 @@
 require "minitest/autorun"
 require_relative "../../models/environment"
 
-class TestEnvironment
+class TestEnvironment < Minitest::Test
   def setup
-
+    @env = Environment.new(
+      "variable_a" => 1,
+      "variable_b" => 2
+    )
   end
 
   def test_evaluate_variables
+    assert_equal(1, @env.evaluate(Variable.new("variable_a")))
+    assert_equal(2, @env.evaluate(Variable.new("variable_b")))
+  end
 
+  def test_evaluate_missing_variable
+    assert_raises(Environment::MissingVariableError) do
+      @env.evaluate(Variable.new("missing_variable"))
+    end
   end
 end
